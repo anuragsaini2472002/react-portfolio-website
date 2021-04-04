4f07f2a6b3249ea6d796623a7775962f90a0edb0
iamanuragsaini <71720957+anuragsaini2472002@users.noreply.github.com>
2021-04-04 21:01:01 +0530

    Add files via upload

---
+++ src/Sdata.js
@@ -0,0 +1,34 @@
+import a11  from './images/a11.jpg'
+import a12  from './images/a12.png'
+import a13  from './images/a13.png'
+import a14  from './images/a14.jpg'
+import a15  from './images/a15.jpg'
+
+
+const Sdata = [
+    {
+        imgSrc : a11,
+        title : 'Web Development'
+    },
+    {
+        imgSrc : a12,
+        title : 'Mobile Development'
+    },
+    {
+        imgSrc : a13,
+        title : 'Blockchain Development'
+    },
+    {
+        imgSrc : a14,
+        title : 'Android  Development'
+    },
+    {
+        imgSrc : a15,
+        title : 'Full-Stack Development'
+    },
+    {
+        imgSrc : a12,
+        title : 'Create Smart Contract'
+    },
+]
+export default Sdata;
